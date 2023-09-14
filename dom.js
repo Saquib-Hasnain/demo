2c799eb95897d17bd561b51edba1dbe54ca1c3d8
Saquib-Hasnain <saquibhasnain786@gmail.com>
2023-09-14 20:09:49 +0530

    third commit

---
+++ dom.js
@@ -0,0 +1,5 @@
+var header = document.getElementById("main-header");
+header.style.borderBottom ='solid 4px black';
+var item = document.getElementsByClassName('title');
+item[0].style.fontWeight='Bold';
+item[0].style.color='green';
